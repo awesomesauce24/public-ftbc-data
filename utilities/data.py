df4a8af3d8517e9fbea528b760a655998b65b6f3
chris s <164194517+awesomesauce24@users.noreply.github.com>
2025-12-19 17:47:15 -0500

    overhaul

---
+++ utilities/data.py
@@ -0,0 +1,71 @@
+# Data support file for FTBC Wiki
+# Contains URLs to all folders in the ftbcdata repository
+
+BASE_URL = "https://github.com/awesomesauce24/ftbcdata/tree/main"
+
+FTBC_CHARACTERS = {
+    "Barren Desert": f"{BASE_URL}/FTBC%20Characters/Barren%20Desert",
+    "Cherry Grove": f"{BASE_URL}/FTBC%20Characters/Cherry%20Grove",
+    "Classic Paradise": f"{BASE_URL}/FTBC%20Characters/Classic%20Paradise",
+    "Evil Forest": f"{BASE_URL}/FTBC%20Characters/Evil%20Forest",
+    "Frozen World": f"{BASE_URL}/FTBC%20Characters/Frozen%20World",
+    "Inverted Realm": f"{BASE_URL}/FTBC%20Characters/Inverted%20Realm",
+    "Magma Canyon": f"{BASE_URL}/FTBC%20Characters/Magma%20Canyon",
+    "Main Realm": f"{BASE_URL}/FTBC%20Characters/Main%20Realm",
+    "Midnight Rooftops": f"{BASE_URL}/FTBC%20Characters/Midnight%20Rooftops",
+    "Motionless": f"{BASE_URL}/FTBC%20Characters/Motionless",
+    "Mythical Forest": f"{BASE_URL}/FTBC%20Characters/Mythical%20Forest",
+    "Nihilism": f"{BASE_URL}/FTBC%20Characters/nihilism",
+    "Polluted Marshlands": f"{BASE_URL}/FTBC%20Characters/Polluted%20Marshlands",
+    "Sakura Serenity": f"{BASE_URL}/FTBC%20Characters/Sakura%20Serenity",
+    "Stairwell": f"{BASE_URL}/FTBC%20Characters/stairwell",
+    "The Backrooms Level": f"{BASE_URL}/FTBC%20Characters/The%20Backrooms%20Level",
+    "The Backrooms Level 0": f"{BASE_URL}/FTBC%20Characters/The%20Backrooms%20Level%200",
+    "The Backrooms Level 1": f"{BASE_URL}/FTBC%20Characters/The%20Backrooms%20Level%201",
+    "The Backrooms Level 153": f"{BASE_URL}/FTBC%20Characters/The%20Backrooms%20Level%20153",
+    "The Backrooms Level 2": f"{BASE_URL}/FTBC%20Characters/The%20Backrooms%20Level%202",
+    "The Backrooms Level 3": f"{BASE_URL}/FTBC%20Characters/The%20Backrooms%20Level%203",
+    "The Backrooms Level 4": f"{BASE_URL}/FTBC%20Characters/The%20Backrooms%20Level%204",
+    "The Backrooms The End": f"{BASE_URL}/FTBC%20Characters/The%20Backrooms%20The%20End",
+    "Timber Peaks": f"{BASE_URL}/FTBC%20Characters/Timber%20Peaks",
+    "Yoyle Factory": f"{BASE_URL}/FTBC%20Characters/Yoyle%20Factory",
+    "Yoyleland": f"{BASE_URL}/FTBC%20Characters/Yoyleland",
+    "Zombie Apocalypse": f"{BASE_URL}/FTBC%20Characters/Zombie%20Apocalypse",
+}
+
+OBJECTS = {
+    "BonusContent": {
+        "Medallions": f"{BASE_URL}/Objects/BonusContent/Medallions",
+        "Secrets": f"{BASE_URL}/Objects/BonusContent/Secrets",
+    },
+    "Realms": {
+        "Backrooms": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/Backrooms.txt",
+        "Barren Desert": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/BarrenDesert.txt",
+        "Cherry Grove": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/CherryGrove.txt",
+        "Classic Paradise": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/ClassicParadise.txt",
+        "Evil Forest": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/EvilForest.txt",
+        "Frozen World": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/FrozenWorld.txt",
+        "Inverted Realm": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/InvertedRealm.txt",
+        "Magma Canyon": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/MagmaCanyon.txt",
+        "Main Realm": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/Main%20Realm.txt",
+        "Midnight Rooftops": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/MidnightRooftops.txt",
+        "Polluted Marshlands": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/PollutedMarshlands.txt",
+        "Sakura Serenity": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/SakuraSerenity.txt",
+        "Timber Peaks": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/TimberPeaks.txt",
+        "Yoyle Factory": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/YoyleFactory.txt",
+        "Yoyleland": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Realms/Yoyleland.txt",
+    },
+    "Subrealms": {
+        "BF1D": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Subrealms/BF1D.txt",
+        "Motionless": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Subrealms/Motionless.txt",
+        "Mythical Forest": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Subrealms/MythicalForest.txt",
+        "Nihilism": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Subrealms/Nihilism.txt",
+        "Stairwell": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Subrealms/Stairwell.txt",
+        "Zombie Apocalypse": "https://github.com/awesomesauce24/ftbcdata/blob/main/Objects/Subrealms/ZombieApocalypse.txt",
+    },
+}
+
+ALL_URLS = {
+    "FTBC_CHARACTERS": FTBC_CHARACTERS,
+    "OBJECTS": OBJECTS,
+}
