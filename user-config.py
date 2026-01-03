15cf32df22db48e2e083e6ef1c43c0af23b6382b
chris s <164194517+awesomesauce24@users.noreply.github.com>
2026-01-03 16:23:40 -0500

    Add password back to user-config.py from environment
    Use exec() to dynamically set password variable from BOT_PASSWORD
    This allows pywikibot to use credentials without prompting

--- user-config.py
+++ user-config.py
@@ -12,16 +12,18 @@ family = 'fandom'
 
 # Setup username from environment
 _bot_user = os.environ.get('BOT_USERNAME')
+_bot_pass = os.environ.get('BOT_PASSWORD')
 
-if _bot_user:
+if _bot_user and _bot_pass:
     # Set username for pywikibot
     from collections import defaultdict as _dd
     _usernames = _dd(lambda: _dd(str))
     _usernames['fandom']['fandom'] = _bot_user
     usernames = _usernames
-
-# pywikibot will automatically read BOT_PASSWORD from environment
-# No need to set password variable here - it's not a standard pywikibot config
+    
+    # Set password directly from environment
+    # This is the variable pywikibot uses for login
+    exec(f"password = '{_bot_pass}'")
 
 # Other settings
 console_encoding = 'utf-8'
